c627db474c16e5335d9532c67a940c0c7405bada
Aaron Wright <aaron.c.wright@icloud.com>
2026-02-16 10:49:57 -0600

    Set up a crt and prg example

---
+++ hellocart/hellocart.asm
@@ -0,0 +1,43 @@
+; 
+; A simple VIC-20 Hello, World!
+;
+
+.segment "STARTUP"
+.segment "CODE"
+
+.include "../VIC-20.inc"
+
+.addr	main			; Cartridge load address
+.addr	main		  ; Address of start-up code
+.byte	$41,$30,$C3,$C2,$CD	; ROM present signature
+
+main:
+  sei                       ; Disable interrupts
+  cld                       ; Clear decimal mode
+  ldx	#$FF                  ; Set stack pointer to $FF
+  txs
+  jsr	RAMTAS
+  jsr	RESTOR
+  jsr	IOINIT
+  jsr	CINT
+  cli
+  
+  lda #8
+  sta VIC_CRF               ; Set background and border to black
+  lda #CHR_WHITE
+  jsr CHROUT                ; Set text color to white
+  lda #CHR_CLEAR            
+  jsr CHROUT                ; Clear the screen
+
+  ldy #0
+loop:
+  lda message,y
+  beq done
+  jsr CHROUT
+  iny
+  jmp loop
+done:
+  jmp done
+
+message:
+  .asciiz "hello vic-20!"   ; Characters need to be lowercase so they are properly mapped to PETSCII
